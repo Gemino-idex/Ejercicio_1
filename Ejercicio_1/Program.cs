967792fd156e465534dff7aad60fce1dd1ee3cb6
Gemino <71484444@idexperujapon.edu.pe>
2025-06-20 16:32:58 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, suma;
+            n1 = 8;
+            n2 = 5;
+            suma = n1 + n2;
+            Console.WriteLine("La suma es:"+suma);
+        }
+    }
+}
